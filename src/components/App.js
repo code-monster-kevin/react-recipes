96fb5bd6c25941b674b0845a903e9365b3aa13e6
Kevin Wu <kevin.wu@live.com.my>
2018-05-12 22:53:57 +0800

    style css basscss cssnext

--- src/components/App.js
+++ src/components/App.js
@@ -35,7 +35,7 @@ class App extends React.Component {
         return(
             <div>
                 <Header />
-                <main style={{ display: 'flex' }}>
+                <main className="px4 flex">
                     <RecipeList 
                         recipes={recipes} 
                         style={{ flex: 3 }}
@@ -43,6 +43,7 @@ class App extends React.Component {
                      />
                     <RecipeDetail 
                         recipe={currentRecipe}
+                        className="ml4"
                         style={{ flex: 5 }} 
                     />
                 </main>
