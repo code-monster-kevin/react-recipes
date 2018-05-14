f78e8c848e53a31075d8a126bcd221f1200e4a0b
Kevin Wu <kevin.wu@live.com.my>
2018-05-14 16:47:05 +0800

    husky with lint and prettier

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const Header = () => (
-    <header className="px4">
-        <h1 className="h1"><span role="img" aria-label="Panda">🐼</span> My Recipes</h1>
-    </header>
+  <header className="px4">
+    <h1 className="h1">
+      <span role="img" aria-label="Panda">
+        🐼
+      </span>{' '}
+      My Recipes
+    </h1>
+  </header>
 );
 
 export default Header;
--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -2,47 +2,45 @@ import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-const RecipeDetail = (props) => {
-    if (!props.recipe) {
-        return (
-            <p style={props.style} className={classNames('h3 p2 bg-white italic center', props.className)} >
-                Select the recipe to see details
-            </p>
-        );
-    }
-
+const RecipeDetail = props => {
+  if (!props.recipe) {
     return (
-        <div style={props.style} className={classNames('p2 bg-white', props.className)}>
-            <h2 className="h2">{props.recipe.name}</h2>
-            <img className="fit" alt={props.recipe.name} src={props.recipe.image} />
-            <div>
-                <span>{props.recipe.category}</span>
-                <span>{props.recipe.calories}</span>
-            </div>
-            <h3>Ingredients</h3>
-            <ul>
-                {props.recipe.ingredients.map(ingredient => (
-                    <li key={ingredient}>
-                        {ingredient}
-                    </li>
-                ))}
-            </ul>
-            <h3>Steps</h3>
-            <ol>
-                {props.recipe.steps.map(step => (
-                    <li key={step}>
-                        {step}
-                    </li>
-                ))}
-            </ol>
-        </div>
+      <p
+        style={props.style}
+        className={classNames('h3 p2 bg-white italic center', props.className)}
+      >
+        Select the recipe to see details
+      </p>
     );
+  }
+
+  return (
+    <div
+      style={props.style}
+      className={classNames('p2 bg-white', props.className)}
+    >
+      <h2 className="h2">{props.recipe.name}</h2>
+      <img className="fit" alt={props.recipe.name} src={props.recipe.image} />
+      <div>
+        <span>{props.recipe.category}</span>
+        <span>{props.recipe.calories}</span>
+      </div>
+      <h3>Ingredients</h3>
+      <ul>
+        {props.recipe.ingredients.map(ingredient => (
+          <li key={ingredient}>{ingredient}</li>
+        ))}
+      </ul>
+      <h3>Steps</h3>
+      <ol>{props.recipe.steps.map(step => <li key={step}>{step}</li>)}</ol>
+    </div>
+  );
 };
 
 RecipeDetail.propTypes = {
-    recipe: PropTypes.object,
-    className: PropTypes.string,
-    style: PropTypes.object,
+  recipe: PropTypes.object,
+  className: PropTypes.string,
+  style: PropTypes.object,
 };
 
 export default RecipeDetail;
