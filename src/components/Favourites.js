bee24f2769fc7b411dc0839cac81d03730ce8c27
Kevin Wu <kevin.wu@live.com.my>
2018-05-15 09:42:59 +0800

    Basic routing

---
+++ src/components/Favourites.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Favourites = () => <h2>Favourites</h2>;
+
+export default Favourites;
--- webpack.config.js
+++ webpack.config.js
@@ -5,36 +5,39 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 require('dotenv').config();
 
 module.exports = {
-    context: path.join(__dirname, 'src'),
-    entry: ['whatwg-fetch','./index.js'],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename:'./bundle.js'
-    },
-    module: {
-        rules:[
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                loader: ['style-loader','css-loader','postcss-loader']
-            },
-            {
-                test: /\.(png|jpeg|jpg|gif)$/,
-                loader: 'file-loader'
-            }
-        ]
-    },
-    plugins:[
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-            inject: 'body'
-        }),
-        new webpack.DefinePlugin({
-            API_URL: JSON.stringify(process.env.API_URL)
-        })
-    ]
+  context: path.join(__dirname, 'src'),
+  entry: ['whatwg-fetch', './index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|jpeg|jpg|gif)$/,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: 'body',
+    }),
+    new webpack.DefinePlugin({
+      API_URL: JSON.stringify(process.env.API_URL),
+    }),
+  ],
 };
